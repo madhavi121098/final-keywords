51884b49d110721dcbb57d1a0f989014ec2a4175
madhavi121098 <114555113+madhavi121098@users.noreply.github.com>
2022-09-30 09:48:14 +0530

    Add files via upload
    final keyword is a constant.

---
+++ Sam1.java
@@ -0,0 +1,11 @@
+class Sam1{
+public static void main(String[]args)
+{
+Sample1 tanya=new Sample1();
+tanya.behavior("Sanjana");
+System.out.println(tanya.behavior("Sanjana"));
+tanya.hobby(6);
+System.out.println(tanya.hobby(6));
+System.out.println(tanya.rollnum);
+}
+}
---
+++ Sample1.java
@@ -0,0 +1,12 @@
+class Sample1
+{
+final long rollnum=5678l;
+public String behavior(String name)
+{
+return name;
+}
+int hobby(int number)
+{
+return number;
+}
+}
